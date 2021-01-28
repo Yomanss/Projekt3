a6d9db7e3012ff78dd0bcc28dfd0b4524dcc7a1c
Yomans <ahmedraad220@outlook.com>
2021-01-28 13:25:42 +0100

    Not much change, still trying to figure out how to make a hamburger menu using js.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
 "use strict";
 
-function Meny() {
-    let subMenu = documnet.getElementById("subMeny");
-    if (subMenu.className == "invisible") {
-        subMenu.className = "menyOn";
-    } 
-    else {
-        subMenu.className = "invisible";
-    }
+let subMenu = documnet.querySelector('.menyOn');
+
+function Meny() {   
+    console.log("Hej");
 }
