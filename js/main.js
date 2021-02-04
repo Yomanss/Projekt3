16866e0b96cbb83939ab19def94d379ca4ce849b
Yomans <ahmedraad220@outlook.com>
2021-02-04 13:30:17 +0100

    Started adding basics for unit conversion

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 "use strict";
 
+let convertFrom = document.querySelector("[data-conversion-from]");
+let numberInput = document.querySelector("[data-number-input]");
+let conversionResult = document.querySelector(".result");
+
+function test() {
+    Event.preventDefault();
+    console.log(numberInput);
+}
+
+convertFrom.addEventListener("submit", test())
