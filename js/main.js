7900b7557f88904da17d2b00fa03c1ee0dcc3802
Yomans <ahmedraad220@outlook.com>
2021-02-11 13:07:36 +0100

    Successfully added the ability to see what type of metric the user inputs

--- js/main.js
+++ js/main.js
@@ -2,12 +2,26 @@
 
 let convertFrom = document.querySelector("[data-conversion-from]");
 let numberInput = document.querySelector("[data-number-input]");
+let inputMetric = document.querySelector("[data-input-select]");
+let outputMetric = document.querySelector("[data-output-select]");
 let conversionResult = document.querySelector("[data-number-output]");
 
 function test() {
     let numberFrom = parseInt(numberInput.value);
-    numberFrom /= 100;
-    conversionResult.value = numberFrom;
+    console.log(inputMetric.value);
+    console.log(outputMetric.value);
+
+    switch (inputMetric.value) {
+        case "centimeter":
+            switch (outputMetric.value) {
+                case "meter":
+                    console.log("HEJ");
+                    numberFrom /= 100;
+                    break;
+            }
+            break;
+    }
+    conversionResult = numberFrom;
 }
 
 convertFrom.addEventListener("submit", (e) => {
