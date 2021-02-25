59ea1fe16474aec6a42c7b9bc96101c745406bd6
Yomans <ahmedraad220@outlook.com>
2021-02-25 20:09:43 +0100

    Added more unit conversion as well as styling

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@
 let unitType = document.querySelector("[data-unit-type]");
 let conversionType = document.getElementsByName("typeRadio");
 let typeLength = document.querySelectorAll(".length");
-let typeTime = document.querySelectorAll(".time");
+let typeTemp = document.querySelectorAll(".temp");
+let typeSpeed = document.querySelectorAll(".speed");
 let radioValue;
 
 let numberInput = document.querySelector("[data-number-input]");
@@ -17,99 +18,215 @@ inputMetric.addEventListener("change", converter);
 outputMetric.addEventListener("change", converter);
 
 function converterType() {
-    
-    for (let i = 0; i < conversionType.length; i++) {
-        if (conversionType[i].checked) {
-          radioValue = conversionType[i].value;  
-        }
+  for (let i = 0; i < conversionType.length; i++) {
+    if (conversionType[i].checked) {
+      radioValue = conversionType[i].value;
     }
+  }
 
+  for (let i = 0; i < typeLength.length; i++) {
+    typeLength[i].classList.remove("invisible");
+  }
+
+  for (let i = 0; i < typeTemp.length; i++) {
+    typeTemp[i].classList.remove("invisible");
+  }
+
+  for (let i = 0; i < typeSpeed.length; i++) {
+    typeSpeed[i].classList.remove("invisible");
+  }
+
+  for (let i = 0; i < typeLength.length; i++) {
+    typeLength[i].classList.add("invisible");
+  }
+
+  for (let i = 0; i < typeTemp.length; i++) {
+    typeTemp[i].classList.add("invisible");
+  }
+
+  for (let i = 0; i < typeSpeed.length; i++) {
+    typeSpeed[i].classList.add("invisible");
+  }
+
+  if (radioValue === "1") {
     for (let i = 0; i < typeLength.length; i++) {
-        typeLength[i].classList.remove("invisible");
+      typeLength[i].classList.remove("invisible");
     }
+    typeTemp[0].removeAttribute("selected");
+    typeTemp[3].removeAttribute("selected");
+    typeSpeed[0].removeAttribute("selected");
+    typeSpeed[3].removeAttribute("selected");
+    typeLength[0].setAttribute("selected", true);
+    typeLength[3].setAttribute("selected", true);
+  }
 
-    for (let i = 0; i < typeTime.length; i++) {
-        typeTime[i].classList.remove("invisible");
+  if (radioValue === "2") {
+    for (let i = 0; i < typeTemp.length; i++) {
+      typeTemp[i].classList.remove("invisible");
     }
+    typeSpeed[0].removeAttribute("selected");
+    typeSpeed[3].removeAttribute("selected");
+    typeLength[0].removeAttribute("selected");
+    typeLength[3].removeAttribute("selected");
+    typeTemp[0].setAttribute("selected", true);
+    typeTemp[3].setAttribute("selected", true);
+  }
 
-    for (let i = 0; i < typeLength.length; i++) {
-        typeLength[i].classList.add("invisible");
+  if (radioValue === "3") {
+    for (let i = 0; i < typeSpeed.length; i++) {
+      typeSpeed[i].classList.remove("invisible");
     }
-
-    for (let i = 0; i < typeTime.length; i++) {
-        typeTime[i].classList.add("invisible");
-    }
-    
-    if (radioValue === "1") {
-        for (let i = 0; i < typeLength.length; i++) {
-            typeLength[i].classList.remove("invisible");
-        }
-        typeTime[0].removeAttribute("selected");
-        typeTime[2].removeAttribute("selected");
-        typeLength[0].setAttribute("selected", true);
-        typeLength[3].setAttribute("selected", true);
-    }
-
-    if (radioValue === "2") {
-        for (let i = 0; i < typeTime.length; i++) {
-            typeTime[i].classList.remove("invisible");
-        }
-        typeLength[0].removeAttribute("selected");
-        typeLength[3].removeAttribute("selected");
-        typeTime[0].setAttribute("selected", true);
-        typeTime[2].setAttribute("selected", true);
-    }
+    typeTemp[0].removeAttribute("selected");
+    typeTemp[3].removeAttribute("selected");
+    typeLength[0].removeAttribute("selected");
+    typeLength[3].removeAttribute("selected");
+    typeSpeed[0].setAttribute("selected", true);
+    typeSpeed[3].setAttribute("selected", true);
+  }
 }
 
 function converter() {
-    let inputValue = parseInt(numberInput.value);
+  let inputValue = parseFloat(numberInput.value);
 
-    switch (inputMetric.value) {
-        case "centimeter":
-            switch (outputMetric.value) {
-                case "centimeter":
-                    inputValue;
-                    break;
-                case "meter":
-                    inputValue /= 100;
-                    break;
-                case "feet":
-                    inputValue /= 30.48;
-                    break;
-            }
-            break;
-        case "meter":
-            switch (outputMetric.value) { 
-                case "centimeter":
-                    inputValue *= 100;
-                    break;
-                case "meter":
-                    inputValue;
-                    break;
-                case "feet":
-                    inputValue *= 3.2808;
-                    break;
-            }
-            break;
-        case "feet":
-            switch(outputMetric.value) {
-                case "centimeter":
-                    inputValue *= 30.48;
-                    break;
-                case "meter":
-                    inputValue /= 3.2808;
-                    break;
-                case "feet":
-                    inputValue;
-                    break;
-            }
-        case "second": 
-            switch(outputMetric.value) {
-                case "second":
-                    inputValue *= 10;
-                    break;
-            }
-            break;
+  switch (inputMetric.value) {
+    case "Centimeter": {
+      switch (outputMetric.value) {
+        case "Centimeter":
+          inputValue;
+          break;
+        case "Meter":
+          inputValue /= 100;
+          break;
+        case "Feet":
+          inputValue /= 30.48;
+          break;
+      }
+      break;
     }
-    numberOutput.value = inputValue.toFixed(2);
+    case "Meter": {
+      switch (outputMetric.value) {
+        case "Centimeter":
+          inputValue *= 100;
+          break;
+        case "Meter":
+          inputValue;
+          break;
+        case "Feet":
+          inputValue *= 3.2808;
+          break;
+      }
+      break;
+    }
+    case "Feet": {
+      switch (outputMetric.value) {
+        case "Centimeter":
+          inputValue *= 30.48;
+          break;
+        case "Meter":
+          inputValue /= 3.2808;
+          break;
+        case "Feet":
+          inputValue;
+          break;
+      }
+      break;
+    }
+    case "Celsius": {
+      switch (outputMetric.value) {
+        case "Celsius":
+          inputValue;
+          break;
+        case "Kelvin":
+          inputValue += 273.15;
+          break;
+        case "Fahrenheit":
+          inputValue *= 1.8;
+          inputValue += 32;
+          break;
+      }
+      break;
+    }
+    case "Kelvin": {
+      switch (outputMetric.value) {
+        case "Celsius":
+          inputValue -= 273.15;
+          break;
+        case "Kelvin":
+          inputValue;
+          break;
+        case "Fahrenheit":
+          inputValue -= 273.15;
+          inputValue *= 1.8;
+          inputValue += 32;
+          break;
+      }
+      break;
+    }
+    case "Fahrenheit": {
+      switch (outputMetric.value) {
+        case "Celsius":
+          inputValue -= 32;
+          inputValue /= 1.8;
+          break;
+        case "Kelvin":
+          inputValue -= 32;
+          inputValue /= 1.8;
+          inputValue += 273.15;
+          break;
+        case "Fahrenheit":
+          inputValue;
+          break;
+      }
+      break;
+    }
+    case "m/s": {
+      switch (outputMetric.value) {
+        case "m/s":
+          inputValue;
+          break;
+        case "km/h":
+          inputValue *= 18;
+          inputValue /= 5;
+          break;
+        case "miles/h":
+          inputValue *= 18;
+          inputValue /= 5;
+          inputValue /= 1.6093;
+          break;
+      }
+      break;
+    }
+    case "km/h": {
+      switch (outputMetric.value) {
+        case "m/s":
+          inputValue *= 5;
+          inputValue /= 18;
+          break;
+        case "km/h":
+          inputValue;
+          break;
+        case "miles/h":
+          inputValue /= 1.6093;
+          break;
+      }
+      break;
+    }
+    case "miles/h": {
+        switch (outputMetric.value) {
+          case "m/s":
+            inputValue *= 1.6093;
+            inputValue *= 5;
+            inputValue /= 18;
+            break;
+          case "km/h":
+            inputValue *= 1.6093;
+            break;
+          case "miles/h":
+            inputValue;
+            break;
+        }
+        break;
+      }
+  }
+  numberOutput.value = inputValue.toFixed(2);
 }
